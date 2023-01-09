6279447a613b0fdf76bc14d040e3890bf8a5eac9
yachi26514153 <cygjr685@yahoo.co.jp>
2023-01-09 07:49:56 +0000

    tasklist1

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,31 @@ Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);
     Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]);
 });
+
+
+
+
+
+
+
+
+
+
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::group(['prefix' => 'users/{id}'], function () {
+        Route::post('follow', [UserFollowController::class, 'store'])->name('user.follow');
+        Route::delete('unfollow', [UserFollowController::class, 'destroy'])->name('user.unfollow');
+        Route::get('followings', [UsersController::class, 'followings'])->name('users.followings');
+        Route::get('followers', [UsersController::class, 'followers'])->name('users.followers');
+        Route::get('favorites', [UsersController::class, 'favorites'])->name('users.favorites');            // 追加
+    });
+    
+    Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);
+    Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]);
+
+    Route::group(['prefix' => 'microposts/{id}'], function () {                                             // 追加
+        Route::post('favorites', [FavoritesController::class, 'store'])->name('favorites.favorite');        // 追加
+        Route::delete('unfavorite', [FavoritesController::class, 'destroy'])->name('favorites.unfavorite'); // 追加
+    });                                                                                                     // 追加
+});
